9231ab19c1f1cea6e1513b7c2ab8aede8eb2062e
Dennyboy94 <dennismuenchehofe@hotmail.de>
2023-03-03 18:35:41 +0100

    Add project files.

---
+++ Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnotherTechblog.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+    }
+}
