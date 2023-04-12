9eb6c326bd2a32a2b7c2f9231273f6f60b5d96d2
DennisM <dennismuenchehofe@hotmail.de>
2023-04-12 22:24:18 +0200

    Added Db and splitted Program into Program and Startup

--- Program.cs
+++ Program.cs
@@ -1,23 +1,8 @@
-using AnotherTechblog.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-/*
-builder.Services.AddDbContext<AnotherTechblogDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AnotherTechblogDbContext") ?? throw new InvalidOperationException("Connection string 'AnotherTechblogDbContext' not found.")));
-*/
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -26,17 +11,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-    endpoints.MapControllerRoute(
-        name: "decryption",
-        pattern: "{controller=Decryption}/{action=DecryptCipher}/{id?}");
-    endpoints.MapControllerRoute(
-        name: "encryption",
-        pattern: "{controller=Encryption}/{action=EncryptCipher}");
-    });
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
---
+++ Startup.cs
@@ -0,0 +1,53 @@
+﻿using AnotherTechblog.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+
+        services.AddDbContext<AnotherTechblogDbContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("YourConnectionString")));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapControllerRoute(
+                name: "decryption",
+                pattern: "{controller=Decryption}/{action=DecryptCipher}/{id?}");
+            endpoints.MapControllerRoute(
+                name: "encryption",
+                pattern: "{controller=Encryption}/{action=EncryptCipher}");
+        });
+    }
+}
